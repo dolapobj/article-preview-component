553cb7ea1cd31dbcde59e3b74dfdcbd814a06ac4
dolapobj <dolapobj@gmail.com>
2024-05-21 18:03:32 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const shareButton = document.getElementById("share-button");
+const footer = document.getElementById("cardFooter");
+const shareBox = document.getElementById('share-box')
+shareButton.addEventListener('click', (e) => {
+    const isMobile = window.matchMedia('(max-width: 768px)').matches;
+    if (isMobile) {
+        footer.classList.toggle('show-share')
+
+    } else {
+     // Get the position of the share button
+    
+shareBox.classList.toggle('show-share')
+
+    }
+})
